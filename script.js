08ea2e64421317aca097db06562b7cabe9e18134
fristonio <deepshpathak@gmail.com>
2017-02-04 15:52:20 +0530

    Started working on rooms

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 window.onload=function(){
-var socket = io();
-console.log(socket.id);
-socket.on('connect', function(){
-  console.log(socket.id);
-});
+	var socket = io();
+	console.log(socket.id);
+	socket.on('connect', function(){
+	  console.log(socket.id);
+	});
 
 
-document.getElementsByTagName('form')[0].onsubmit=function(){
-	var link=document.getElementsByTagName('input')[0].value ;
-	link=link.replace('/watch?v=','/embed/');
-	link=link+"?autoplay=1";
-	socket.emit('dataemit',{'link': link , 'time':Date.now()});
-	document.getElementById("mainvid").src=link;
-	return false;
-};
+	document.getElementsByTagName('form')[0].onsubmit=function(){
+		var link=document.getElementsByTagName('input')[0].value ;
+		link=link.replace('/watch?v=','/embed/');
+		link=link+"?autoplay=1";
+		socket.emit('dataemit',{'link': link , 'time':Date.now()});
+		document.getElementById("mainvid").src=link;
+		return false;
+	};
 
-socket.on('playnow',function(data){
-	var t=(Date.now()-data.time)/1000;
-	t=parseInt(t)+1;
-	console.log(t);
-	var link=data.link+"&start="+t;
-	console.log(link);
-	document.getElementById("mainvid").src=link;
-});
-document.getElementsByClassName('sync')[0].onclick=function(){
-	socket.emit('sync');
-}
+	socket.on('playnow',function(data){
+		//var t=(Date.now()-data.time)/1000;
+		//t=parseInt(t)+1;
+		t=data.curtime;
+		console.log(t);
+		var link=data.link+"&start="+t;
+		console.log(link);
+		document.getElementById("mainvid").src=link;
+	});
+	document.getElementsByClassName('sync-btn')[0].onclick=function(){
+		socket.emit('sync');
+	}
+
+	document.getElementsByClassName('join')[0].onclick=function(){
+		var jr=document.getElementsByClassName('join-room')[0];
+		jr.style.zIndex="5";
+		jr.style.display="flex";
+	};
+
+	document.getElementsByClassName("close")[0].onclick=function(){
+		this.parentNode.style.display="none";
+	};
+
+
 };
--- stream.js
+++ stream.js
@@ -1,16 +1,20 @@
 var http=require('http');
 var event=require('events');
 var express=require('express');
-var bodyParser = require('body-parser');//will be required if we do form data send receiving through express
+var fs=require('fs');
+//var bodyParser = require('body-parser');//will be required if we do form data send receiving through express
 var playing=false; //wheather a video is being played or not
 var userCount=0;  // no of user connected to the room at present
 var users=[]; //users connected to the server at the moment 
-var nowplay={'link':'','time':Date.now()}; //data of the video being played at the current moment
+var nowplay={'link':'','time':Date.now(),'curtime':0}; //data of the video being played at the current moment
 var app=express();
 
-
-app.use(bodyParser.json());// to work with the get and post request form data with the help of the express library in nodejs
-app.use(bodyParser.urlencoded({ extended: true }));
+var rooms={
+	'public':{'name':'Public','userCount':0, 'nowplay':{'link':'','time':Date.now(),'curtime':0} , },
+	'private':[]
+};
+//app.use(bodyParser.json());// to work with the get and post request form data with the help of the express library in nodejs
+//app.use(bodyParser.urlencoded({ extended: true }));
 
 
 var server=http.createServer(app,function(req,res){
@@ -41,23 +45,22 @@ app.get('/script.js', function(req, res) {
 
 var io = require('socket.io')(server);
 io.on('connection', function(client){
-	//console.log(client);
 	users.push(client.id);
 	userCount+=1;
 	if(playing==true){
+		var t=(Date.now()-nowplay.time)/1000;
+		t=parseInt(t)+1;
+		nowplay.curtime=t;
 		io.emit('playnow',nowplay);
 	}
 	console.log('No of user connected to the server are   :  '+userCount);
-	//io.emit('event1');
-	/*if((Date.now()-nowplay.time)>30000){
-		nowplay.time=Date.now();
-	}*/
+
 
   	client.on('dataemit', function(data){
   		//if (playing==false) {
   		console.log('Link given by the user is  :  '+data.link);
-  		console.log('Unique timestamp :  '+data.time);
-  		console.log(data);
+  		//console.log('Unique timestamp :  '+data.time);
+  		//console.log(data);
   		nowplay.link=data.link;
   		nowplay.time=data.time;
   		console.log("currently playing video is  :  "+nowplay.link);
@@ -77,6 +80,13 @@ io.on('connection', function(client){
   	/*client.on('getinfo',function(){
   		console.log(users);
   	});*/
+  	client.on('sync', function(){
+  		var t=(Date.now()-nowplay.time)/1000;
+		t=parseInt(t)+1;
+		nowplay.curtime=t;
+		io.emit('playnow',nowplay);
+  	});
+
   	console.log(users);
 });
 
